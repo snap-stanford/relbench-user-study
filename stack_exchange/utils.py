b6cd5d48918b4c4d86c439d773912def6fe82e4a
adobles96 <alejandro.d.96@gmail.com>
2024-04-19 18:57:55 -0700

    Add db_setup method to utils

--- stack_exchange/utils.py
+++ stack_exchange/utils.py
@@ -1,15 +1,45 @@
+import duckdb
+from jinja2 import Template
 import pandas as pd
+from relbench.datasets import get_dataset
 from sklearn.feature_selection import mutual_info_classif
 
 
-def feature_summary_df(val_df: pd.DataFrame, y_col: str) -> pd.DataFrame:
-    y = val_df[y_col]
-    val_df = val_df.drop(y_col, axis=1)
+def db_setup(db_name: str, cache_dir: str | None = None):
+    conn = duckdb.connect(db_name)
+    dataset = get_dataset(name='rel-math-stackex', process=True, cache_dir=cache_dir)
+    users = dataset.db.table_dict['users'].df  # noqa
+    posts = dataset.db.table_dict['posts'].df  # noqa
+    votes = dataset.db.table_dict['votes'].df  # noqa
+    badges = dataset.db.table_dict['badges'].df  # noqa
+    comments = dataset.db.table_dict['comments'].df  # noqa
+    conn.sql('create table users as select * from users')
+    conn.sql('create table posts as select * from posts')
+    conn.sql('create table votes as select * from votes')
+    conn.sql('create table badges as select * from badges')
+    conn.sql('create table comments as select * from comments')
+    for task_name in ['engage', 'badges', 'votes']:
+        task = dataset.get_task(f'rel-stackex-{task_name}', process=True)
+        train_table = task.train_table.df  # noqa
+        val_table = task.val_table.df  # noqa
+        test_table = task.test_table.df  # noqa
+        conn.sql(f'create table {task_name}_train as select * from train_table')
+        conn.sql(f'create table {task_name}_val as select * from val_table')
+        conn.sql(f'create table {task_name}_test as select * from test_table')
+
+
+def render_jinja_sql(query: str, context: dict) -> str:
+    return Template(query).render(context)
+
+
+def feature_summary_df(df: pd.DataFrame, y_col: str) -> pd.DataFrame:
+    y = df[y_col]
+    df = df.drop(y_col, axis=1)
     return pd.DataFrame(
         {
-            'Label Corr.': val_df.corrwith(y),
-            'Label MI': mutual_info_classif(val_df.fillna(-1).values, y),
-            'NaN %': val_df.isna().mean(),
+            'Label Corr.': df.corrwith(y),
+            'Label MI': mutual_info_classif(df.fillna(-1).values, y),
+            'NaN %': df.isna().mean(),
         },
-        index=val_df.columns
+        index=df.columns
     ).sort_values(by='Label Corr.', ascending=False)
