382a6f4b06ae7df64d30d7150fce5a08a35ba06a
adobles96 <alejandro.d.96@gmail.com>
2024-05-22 14:36:50 -0700

    Fix bug in db_setup

--- utils.py
+++ utils.py
@@ -22,22 +22,22 @@ DATASET_INFO = {
 }
 
 
-def db_setup(dataset: str, db_filename: str):
+def db_setup(dataset_name: str, db_filename: str):
     """ Sets up a DuckDB database (at db_filename) with the tables from the specified dataset.
 
     Args:
-        dataset (str): The name of the relbench dataset.
+        dataset_name (str): The name of the relbench dataset.
         db_filename (str): Path to the DuckDB database file.
     """
     conn = duckdb.connect(db_filename)
-    dataset = get_dataset(name=dataset, process=True)
-    tasks = DATASET_INFO[dataset]['tasks']
-    tables = DATASET_INFO[dataset]['tables']
+    dataset = get_dataset(name=dataset_name, process=True)
+    tasks = DATASET_INFO[dataset_name]['tasks']
+    tables = DATASET_INFO[dataset_name]['tables']
     for table_name in tables:
         exec(f'{table_name} = dataset.db.table_dict["{table_name}"].df')
         conn.sql(f'create table {table_name} as select * from {table_name}')
     for task_name in tasks:
-        task = dataset.get_task(f'{dataset}-{task_name}', process=True)
+        task = dataset.get_task(f'{dataset_name}-{task_name}', process=True)
         train_table = task.train_table.df  # noqa
         val_table = task.val_table.df  # noqa
         test_table = task.test_table.df  # noqa
