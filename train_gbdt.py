d68b112088f37ad5111af584d9396a44fe9c86d6
adobles96 <alejandro.d.96@gmail.com>
2024-07-17 14:33:45 -0700

    Fix quotation mark bug

--- train_gbdt.py
+++ train_gbdt.py
@@ -217,10 +217,10 @@ if __name__ == '__main__':
     pred = gbdt.predict(tf_test=val_tf).numpy()
     assert len(task.get_table("val").df) == len(val_df), 'Val: feature df does not match label df!'
     pred = map_preds(val_df, task.get_table("val").df, task_params['identifier_cols'], pred)
-    print(f"Val: {task.evaluate(pred, task.get_table("val"))}")
+    print(f'Val: {task.evaluate(pred, task.get_table("val"))}')
     print()
     test_tf = train_dset.convert_to_tensor_frame(test_df)
     assert len(task.get_table("test").df) == len(test_df), 'Test: feature df does not match label df!'
     pred = gbdt.predict(tf_test=test_tf).numpy()
     pred = map_preds(test_df, task.get_table("test").df, task_params['identifier_cols'], pred)
-    print(f"Test: {task.evaluate(pred)}")
+    print(f'Test: {task.evaluate(pred)}')
